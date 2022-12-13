b52acb52db45b941ba8336674b40e1469873bcd1
BaiPutra <baihaqi917@gmail.com>
2022-12-13 16:23:44 +0700

    add materi session 4

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,30 +1,46 @@
 import "./App.css";
 import React from "react";
+import { useState } from 'react';
 
-class Item extends React.Component {
-  render() {
-    return <h2>This is the item component</h2>;
-  }}
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {name: 'John'}
+const Form = () => {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState();
+  const [gender, setGender] = useState("laki-laki")
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const dataDiri = { name, age, gender }
   }
-  changeName = () => {
-    this.setState({name: 'Baihaqi'})
-  }
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello my name is {this.state.name}</h1>
-        <Item />
-        <button
-          type="button"
-          onClick={this.changeName}>
-          Change Name
-        </button>
-     </div>);
-  }}
 
-export default App;
+  return (
+    <div className="create">
+      <h2>Isi data diri anda</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="">Nama anda:</label>
+        <input 
+          type="text"
+          required
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <label>Umur anda:</label>
+        <input 
+          type="number"
+          required
+          value={age}
+          onChange={(e) => setAge(e.target.value)}
+        />
+        <label>Jenis Kelamin:</label>
+        <select value={gender} onChange={(e) => {setGender(e.target.value)}}>
+          <option value="laki-laki">Laki-laki</option>
+          <option value="perempuan">Perempuan</option>
+        </select>
+        <label>Hobi:</label>
+        <textarea />
+        <button>Submit Data</button>
+      </form>
+    </div>
+  )
+}
+
+export default Form;
