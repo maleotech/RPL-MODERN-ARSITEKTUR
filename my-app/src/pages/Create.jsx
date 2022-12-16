790c2eb139ac0d420fbde6ffcd92d2c8187afa1f
BaiPutra <baihaqi917@gmail.com>
2022-12-16 16:19:58 +0700

    add materi session 5

---
+++ my-app/src/pages/Create.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+
+const Create = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("cristiano");
+  const [isLoading, setIsLoading] = useState(false)
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author }
+
+    setIsLoading(true)
+
+    fetch('http://localhost:8000/blogs', {
+      method: 'POST',
+      body: JSON.stringify(blog),
+      headers: {'Content-type' : 'application/json'},
+    })
+    .then(() => {
+      console.log('new blog added');
+      setIsLoading(false)
+    })
+  }
+
+  return (
+    <div className="create">
+      <h2>Add a New Blog</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Blog title:</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Blog body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        />
+        <label>Blog author:</label>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+          <option value="cistiano">cristiano</option>
+          <option value="messi">messi</option>
+        </select>
+        { !isLoading && <button>Add Blog</button> }
+        { isLoading && <button>Adding blog...</button> }
+      </form>
+    </div>
+  );
+};
+
+export default Create;
