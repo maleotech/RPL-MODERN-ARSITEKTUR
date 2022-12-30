49f5ead0989088ec18829319a586d2504801334d
BaiPutra <baihaqi917@gmail.com>
2022-12-30 16:53:56 +0700

    update materi session 3

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,30 +1,62 @@
 import "./App.css";
 import React from "react";
 
-class Item extends React.Component {
-  render() {
-    return <h2>This is the item component</h2>;
-  }}
-
-class App extends React.Component {
+export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {name: 'John'}
+    this.state = {
+      hitung: 0, // state hitung default
+    };
   }
-  changeName = () => {
-    this.setState({name: 'Baihaqi'})
-  }
+
+  // function menambahkan nilai state
+  tambahState = () => {
+    this.setState({ hitung: this.state.hitung + 1 })};
+
+  // function mengurangkan nilai state
+  kurangState = () => {
+    if (this.state.hitung > 0) {    // pastikan nilai state lebih dari 0
+      this.setState({ hitung: this.state.hitung - 1 });
+    } else {
+      alert("Pengurangan State Telah Mencapai Batas")
+      this.setState({ hitung: this.state.hitung });
+    }};
+
   render() {
     return (
       <div className="App">
-        <h1>Hello my name is {this.state.name}</h1>
-        <Item />
-        <button
-          type="button"
-          onClick={this.changeName}>
-          Change Name
-        </button>
-     </div>);
-  }}
+        <h1>Hitung State</h1>
+        <h3>Nilai State Sekarang : {this.state.hitung}</h3>
+        <p>*nilai state tidak boleh minus</p>
+        <button onClick={this.tambahState}>Tambah State</button>
+        <button onClick={this.kurangState}>Kurang State</button>
+      </div>
+    )}}
 
-export default App;
+// class Item extends React.Component {
+//   render() {
+//     return <h2>This is the item component</h2>;
+//   }}
+
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {name: 'John'}
+//   }
+//   changeName = () => {
+//     this.setState({name: 'Baihaqi'})
+//   }
+//   render() {
+//     return (
+//       <div className="App">
+//         <h1>Hello my name is {this.state.name}</h1>
+//         <Item />
+//         <button
+//           type="button"
+//           onClick={this.changeName}>
+//           Change Name
+//         </button>
+//      </div>);
+//   }}
+
+// export default App;
